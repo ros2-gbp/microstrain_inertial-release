5ac80db3eb1f316d973fbcb0797254aef6622ea4
robbiefish <rob.fisher@hbkworld.com>
2024-04-04 16:46:49 -0400

    Rebase from 'upstream'

--- microstrain_inertial_driver/launch/microstrain_launch.py
+++ microstrain_inertial_driver/launch/microstrain_launch.py
@@ -5,13 +5,9 @@
 import os
 import yaml
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, EmitEvent, RegisterEventHandler
-from launch.conditions import LaunchConfigurationEquals
-from launch.substitutions import LaunchConfiguration, PythonExpression
-from launch.events import matches_action
-from launch.event_handlers import OnProcessStart, OnProcessExit, OnExecutionComplete
-from launch_ros.actions import LifecycleNode
-from launch_ros.events.lifecycle import ChangeState
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 from lifecycle_msgs.msg import Transition
 from ament_index_python.packages import get_package_share_directory
@@ -35,8 +31,6 @@ def generate_launch_description():
   launch_description = []
   launch_description.append(DeclareLaunchArgument('namespace',   default_value='/',                description='Namespace to use when launching the nodes in this launch file'))
   launch_description.append(DeclareLaunchArgument('node_name',   default_value=_PACKAGE_NAME,      description='Name to give the Microstrain Inertial Driver node'))
-  launch_description.append(DeclareLaunchArgument('configure',   default_value='true',             description='Whether or not to configure the node on startup'))
-  launch_description.append(DeclareLaunchArgument('activate',    default_value='true',             description='Whether or not to activate the node on startup'))
   launch_description.append(DeclareLaunchArgument('debug',       default_value='false',            description='Whether or not to log debug information.'))
   launch_description.append(DeclareLaunchArgument('params_file', default_value=_EMPTY_PARAMS_FILE, description='Path to file that will load additional parameters'))
 
@@ -46,7 +40,7 @@ def generate_launch_description():
   # ****************************************************************** 
   # Microstrain sensor node 
   # ****************************************************************** 
-  microstrain_node = LifecycleNode(
+  microstrain_node = Node(
     package    = _PACKAGE_NAME,
     executable = "microstrain_inertial_driver_node",
     name       = LaunchConfiguration('node_name'),
@@ -65,43 +59,7 @@ def generate_launch_description():
     ]
   )
 
-  # Optional configure and activate steps
-  config_event = EmitEvent(
-    event = ChangeState(
-      lifecycle_node_matcher = matches_action(microstrain_node),
-      transition_id          = Transition.TRANSITION_CONFIGURE
-    ),
-    condition = LaunchConfigurationEquals('configure', 'true')
-  )
-  activate_event = EmitEvent(
-    event = ChangeState(
-      lifecycle_node_matcher = matches_action(microstrain_node),
-      transition_id          = Transition.TRANSITION_ACTIVATE
-    ),
-    condition = LaunchConfigurationEquals('activate', 'true')
-  )
-
-  # String the event emitions together so that one happens after the other
-  config_event_handler = RegisterEventHandler(
-    OnProcessStart(
-      target_action=microstrain_node,
-      on_start=[
-        config_event
-      ]
-    )
-  )
-  activate_event_handler = RegisterEventHandler(
-    OnExecutionComplete(
-      target_action=config_event,
-      on_completion=[
-        activate_event
-      ]
-    )
-  )
-
   launch_description.append(microstrain_node)
-  launch_description.append(config_event_handler)
-  launch_description.append(activate_event_handler)
   return LaunchDescription(launch_description)
   
 
