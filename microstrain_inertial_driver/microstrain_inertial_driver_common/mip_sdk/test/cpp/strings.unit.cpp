2b939bef7b9a94209cc44156ba9255c800f52032
Aidan Laracy <aidan.laracy@hbkworld.com>
2026-06-09 15:01:43 -0400

    Rebase from 'upstream'

---
+++ microstrain_inertial_driver/microstrain_inertial_driver_common/mip_sdk/test/cpp/strings.unit.cpp
@@ -0,0 +1,77 @@
+#define MICROSTRAIN_HAS_STD_STRING 1
+
+#include <string_view>
+
+#include <microstrain/strings.hpp>
+#include <microstrain_test/microstrain_test.hpp>
+
+static constexpr char CHECK_STRING[] = "Test: \"quotes\" 'single' & <xml/> {json} [array] $100 €50 ¥200 @user #tag 🚀 \n\t\\ 100% café naïve";
+[[maybe_unused]] static constexpr size_t CHECK_STRING_LENGTH = sizeof(CHECK_STRING) - 1; // Length without null terminator
+
+MICROSTRAIN_TEST_CASE("C++ string concatenation", "A char array view can be concatenated to a buffer")
+{
+    char buffer[1024] = {};
+    size_t index = 0;
+
+    const bool ok = microstrain::strings::concat(buffer, &index, microstrain::ConstCharArrayView{CHECK_STRING});
+
+    CHECK(ok);
+    CHECK_CSTR_EQ(buffer, CHECK_STRING);
+}
+
+MICROSTRAIN_TEST_CASE("C++ string concatenation", "A std::string_view can be concatenated to a buffer")
+{
+    char buffer[1024] = {};
+    size_t index = 0;
+
+    const bool ok = microstrain::strings::concat(buffer, &index, std::string_view{CHECK_STRING});
+
+    CHECK(ok);
+    CHECK_CSTR_EQ(buffer, CHECK_STRING);
+}
+
+MICROSTRAIN_TEST_CASE("C++ string concatenation", "A std::string can be concatenated to a buffer")
+{
+    char buffer[1024] = {};
+    size_t index = 0;
+
+    const bool ok = microstrain::strings::concat(buffer, &index, std::string{CHECK_STRING});
+
+    CHECK(ok);
+    CHECK_CSTR_EQ(buffer, CHECK_STRING);
+}
+
+MICROSTRAIN_TEST_CASE("C++ string concatenation", "A zero-terminated C string can be concatenated to a buffer")
+{
+    char buffer[1024] = {};
+    size_t index = 0;
+
+    const bool ok = microstrain::strings::concat_cstr(buffer, &index, CHECK_STRING);
+
+    CHECK(ok);
+    CHECK_CSTR_EQ(buffer, CHECK_STRING);
+}
+
+MICROSTRAIN_TEST_CASE("C++ string concatenation", "Up to N characters of a zero-terminated string can be concatenated to a buffer")
+{
+    char buffer[1024] = {};
+    size_t index = 0;
+    constexpr size_t character_limit = 4;
+
+    const bool ok = microstrain::strings::concat_cstr(buffer, &index, "123456789", character_limit);
+
+    CHECK(ok);
+    CHECK_EQ(buffer[character_limit], '\0');
+    CHECK_CSTR_EQ(buffer, "1234");
+}
+
+MICROSTRAIN_TEST_CASE("C++ string concatenation", "A string literal can be concatenated to a buffer")
+{
+    char buffer[1024] = {};
+    size_t index = 0;
+
+    const bool ok = microstrain::strings::concat_l(buffer, &index, "123456789");
+
+    CHECK(ok);
+    CHECK_CSTR_EQ(buffer, "123456789");
+}
